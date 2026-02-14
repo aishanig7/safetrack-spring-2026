8adbe10511d7512cce8e0dea2a67b9f9d241546f
AishaniG7 <aishani.gupta.2007@gmail.com>
2026-02-13 17:24:36 -0700

    Mesh Packet

---
+++ src/meshPacket.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+
+#define PKT_GPS  2
+#define PKT_HELLO 1
+#define MESH_BROADCAST 0xFF
+
+struct MeshPacket {
+  uint8_t src;
+  uint8_t lastHop;
+  uint8_t dst;
+  uint8_t ttl;
+  uint16_t seq;
+  uint8_t type;
+  uint8_t payload[32];
+};
